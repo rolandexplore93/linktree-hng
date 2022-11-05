79d428e34d90a79bd114a48498cd912ac976ddba
rolandexplore93 <roland2rule@gmail.com>
2022-11-05 11:56:51 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AnimatedRoutes from './AnimatedRoutes';
-import layouts from './layout';
 
 function App() {
   return (
